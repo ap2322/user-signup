fc29da17bd0652d73834765d45319158ec66309e
ap2322 <alicep2322@gmail.com>
2018-07-16 21:43:51 -0600

    stashing

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True 
 
-@app.route('/confirmation', methods = ['GET', 'POST'])
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def index():   
+#    encoded_error = request.args.get("error")
+    username_error = request.args.get("username_error")
+    password_error = request.args.get("password_error")
+    password_verification_error = request.args.get("password-verification_error")
+    return render_template('userinfo.html', 
+#        error=encoded_error and cgi.escape(encoded_error, quote=True), 
+        username_error=username_error and cgi.escape(username_error, quote=True),
+        password_error=password_error and cgi.escape(password_error, quote=True),
+        password_verification_error=password_verification_error and cgi.escape(password_verification_error, quote=True)
+        )
+
+@app.route('/userinfo', methods = ['GET', 'POST'])
 def verification():
     username = request.form['username']
     username_error = "Please enter a username greater than 3 and less than 20 characters without spaces."
@@ -52,20 +67,6 @@ def verification():
 
     return render_template('/confirmation.html', username = username, username_error = '', password_error = '')
 
-
-@app.route("/", methods = ['GET', 'POST'])
-def index():   
-#    encoded_error = request.args.get("error")
-    username_error = request.args.get("username_error")
-    password_error = request.args.get("password_error")
-    password_verification_error = request.args.get("password-verification_error")
-    return render_template('userinfo.html', 
-#        error=encoded_error and cgi.escape(encoded_error, quote=True), 
-        username_error=username_error and cgi.escape(username_error, quote=True),
-        password_error=password_error and cgi.escape(password_error, quote=True),
-        password_verification_error=password_verification_error and cgi.escape(password_verification_error, quote=True)
-        )
-
 # TODO: make a verification function for each field: username, password, verify password
 # email
 
@@ -77,7 +78,7 @@ def index():
 # TODO: email cannot be empty; has a single @; has a single '.' after the @; 
 
 
-@app.route('/confirmation', methods = ['POST'])
+@app.route('/userinfo', methods = ['POST'])
 def confirm_user():
     username = request.form['username']
     return render_template("/confirmation.html", username = username)
